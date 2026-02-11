773cad1ba9b7a91fae33bb547269b561687c2bc5
unknown <jescalesjr1987@gmail.com>
2026-02-10 23:17:29 -0500

    uploading tf config files

---
+++ 1.vpc.tf
@@ -0,0 +1,10 @@
+# this  makes  vpc.id which is aws_vpc.app1.id
+resource "aws_vpc" "dev" {
+  cidr_block = "10.10.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name    = "dev1"
+  }
+}
